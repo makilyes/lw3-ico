3f92bf4432f04be133f31457ad4d8f47dc82348e
makilyes <mak_titi@hotmail.com>
2022-12-07 16:02:18 +0100

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x7e6Bc42f65f14f00438E20140b27D71694Be26dF";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://lw3-nft-collection-aznz.vercel.app/api/";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS, METADATA_URL };
